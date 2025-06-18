ab4def182272cd05abec7ed94d7f0aa09e8433c9
Kobi Hari <kobi2294@yahoo.com>
2025-06-18 14:39:03 +0300

    Icons in the application

--- solar-ws/projects/solar-lib/src/providers/provide-lib.ts
+++ solar-ws/projects/solar-lib/src/providers/provide-lib.ts
@@ -1,11 +1,14 @@
 import { EnvironmentProviders, inject, makeEnvironmentProviders, provideAppInitializer } from "@angular/core";
 import { delay, of } from "rxjs";
 import { InitService } from "../services/init.service";
+import { provideHttpClient } from "@angular/common/http";
 
 export function provideLib(): EnvironmentProviders {
     return makeEnvironmentProviders([
+        provideHttpClient(),
         provideAppInitializer(() => {
             inject(InitService).init();
-        })
+        }), 
+
     ])
 }
--- solar-ws/projects/solar-lib/src/services/init.service.ts
+++ solar-ws/projects/solar-lib/src/services/init.service.ts
@@ -1,15 +1,20 @@
 import { inject, Injectable } from "@angular/core";
 import { MatIconRegistry } from "@angular/material/icon";
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Injectable({providedIn: 'root'})
 export class InitService {
     readonly matIconRegistry = inject(MatIconRegistry);
+    readonly domSanitizer = inject(DomSanitizer);
 
 
     init() {
+        const illustrationIcon = this.domSanitizer.bypassSecurityTrustResourceUrl('Illustration.svg');
+
+
         this.matIconRegistry.setDefaultFontSetClass('material-symbols-outlined');
         this.matIconRegistry.registerFontClassAlias('icons', 'material-icons-sharp');
-        this.matIconRegistry.addSvgIcon('flower', 'illustration.svg');
+        this.matIconRegistry.addSvgIcon('flower', illustrationIcon);
 
     }
 }

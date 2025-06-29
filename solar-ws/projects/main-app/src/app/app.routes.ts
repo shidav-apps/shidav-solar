8d4ab34a3ac43b7bb4a7ecbe92e5acf40b090750
Kobi Hari <kobi2294@yahoo.com>
2025-06-29 11:08:12 +0300

    initial pages and routing structure

--- solar-ws/projects/main-app/src/app/app.routes.ts
+++ solar-ws/projects/main-app/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', loadComponent: () => import('./pages/login/login.component')}, 
+    {path: 'user', loadComponent: () => import('./pages/user/user.component'), 
+        children: [
+            { path: 'dashboard', loadComponent: () => import('./pages/user/pages/dashboard/dashboard.component') },
+            { path: 'invoice', loadComponent: () => import('./pages/user/pages/invoice/invoice.component') },
+            { path: 'site', loadComponent: () => import('./pages/user/pages/site/site.component') },
+        ]
+    },
+];
---
+++ solar-ws/projects/main-app/src/app/pages/user/pages/site/site.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-site',
+  imports: [],
+  templateUrl: './site.component.html',
+  styleUrl: './site.component.scss'
+})
+export default class SiteComponent {
+
+}

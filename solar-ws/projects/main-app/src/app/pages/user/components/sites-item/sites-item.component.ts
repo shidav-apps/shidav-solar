40e46269f28e95d57d6b15f5ee865bdac6a57356
Kobi Hari <kobi2294@yahoo.com>
2025-08-20 11:24:38 +0300

    styling of sites list

--- solar-ws/projects/main-app/src/app/pages/user/components/sites-item/sites-item.component.ts
+++ solar-ws/projects/main-app/src/app/pages/user/components/sites-item/sites-item.component.ts
@@ -2,12 +2,17 @@ import { Component, inject, input } from '@angular/core';
 import { SiteInfo } from '@contract';
 import { SiteVm } from '../sites-list/store/view-model/site.vm';
 import { SitesListStore } from '../sites-list/store/sites-list.store';
+import { MatButtonModule } from "@angular/material/button";
+import { SharedModule } from '@solar-lib';
 
 @Component({
   selector: 'app-sites-item',
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './sites-item.component.html',
-  styleUrl: './sites-item.component.scss'
+  styleUrl: './sites-item.component.scss', 
+  host: {
+    '[class.selected]': 'store.selectedSiteId() === site().id',
+  }
 })
 export class SitesItemComponent {
   readonly site = input.required<SiteVm>();

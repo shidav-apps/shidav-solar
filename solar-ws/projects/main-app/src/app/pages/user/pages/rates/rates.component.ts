5531a213538c3c294ec0dff4a76ae156f5bed577
Kobi Hari <kobi2294@yahoo.com>
2025-11-05 16:36:18 +0200

    lib table demo

--- solar-ws/projects/main-app/src/app/pages/user/pages/rates/rates.component.ts
+++ solar-ws/projects/main-app/src/app/pages/user/pages/rates/rates.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { LibTableColumnDefinition, LibTableModule } from '@solar-lib';
+import { SAMPLE_DATA, SampleRecord } from './sample-data';
 
 @Component({
   selector: 'app-rates',
-  imports: [],
+  imports: [LibTableModule],
   templateUrl: './rates.component.html',
   styleUrl: './rates.component.scss'
 })
 export default class RatesComponent {
+  readonly columns: LibTableColumnDefinition<SampleRecord>[] = [
+    { id: 'name', header: 'שם', value: r => r.name, width: 200 }, 
+    { id: 'phone', header: 'טלפון', value : r => r.phone, width: 120 },
+    { id: 'email', header: 'אימייל', value: r => r.email, width: 250 },
+    { id: 'area', header: 'אזור', value: r => r.area, width: 100 },
+    { id: 'supplier', header: 'ספק', value: r => r.supplier, width: 100 },
+    { id: 'accountNumber', header: 'מספר חשבון', value: r => r.accountNumber, width: 150 },
+  ] as const;
+
+  readonly data = signal(SAMPLE_DATA);
 
 }
---
+++ solar-ws/projects/main-app/src/app/pages/user/pages/rates/sample-data.ts
@@ -0,0 +1,210 @@
+export interface SampleRecord {
+    readonly id: string;    
+    readonly name: string;
+    readonly phone: string;
+    readonly email: string;
+    readonly area: string;
+    readonly supplier: string;
+    readonly accountNumber: string;
+}
+
+export const SAMPLE_DATA: SampleRecord[] = [
+    {
+        id: '1231', 
+        name: 'משה יציקוביץ', 
+        phone: '050-1234567',
+        email: 'moshe-yetz@gmail.com', 
+        area: 'מרכז', 
+        supplier: '50', 
+        accountNumber: '62342-12'
+    },
+    {
+        id: '1232',
+        name: 'רחל כהן',
+        phone: '052-9876543',
+        email: 'rachel.cohen@gmail.com',
+        area: 'צפון',
+        supplier: '51',
+        accountNumber: '62342-13'
+    },
+    {
+        id: '1233',
+        name: 'דוד לוי',
+        phone: '054-5551234',
+        email: 'david.levi@outlook.com',
+        area: 'דרום',
+        supplier: '52',
+        accountNumber: '62342-14'
+    },
+    {
+        id: '1234',
+        name: 'שרה אברהם',
+        phone: '050-7778888',
+        email: 'sarah.avraham@hotmail.com',
+        area: 'מרכז',
+        supplier: '53',
+        accountNumber: '62342-15'
+    },
+    {
+        id: '1235',
+        name: 'יוסף דהן',
+        phone: '053-2223333',
+        email: 'yosef.dahan@gmail.com',
+        area: 'ירושלים',
+        supplier: '54',
+        accountNumber: '62342-16'
+    },
+    {
+        id: '1236',
+        name: 'מרים גרוס',
+        phone: '052-4445555',
+        email: 'miriam.gross@walla.co.il',
+        area: 'צפון',
+        supplier: '50',
+        accountNumber: '62342-17'
+    },
+    {
+        id: '1237',
+        name: 'אברהם פרידמן',
+        phone: '054-6667777',
+        email: 'avraham.friedman@gmail.com',
+        area: 'מרכז',
+        supplier: '51',
+        accountNumber: '62342-18'
+    },
+    {
+        id: '1238',
+        name: 'לאה רוזנברג',
+        phone: '050-8889999',
+        email: 'lea.rosenberg@bezeq.net',
+        area: 'דרום',
+        supplier: '52',
+        accountNumber: '62342-19'
+    },
+    {
+        id: '1239',
+        name: 'יעקב שמיר',
+        phone: '053-1112222',
+        email: 'yaakov.shamir@gmail.com',
+        area: 'השרון',
+        supplier: '53',
+        accountNumber: '62342-20'
+    },
+    {
+        id: '1240',
+        name: 'רבקה בן דוד',
+        phone: '052-3334444',
+        email: 'rivka.bendavid@hotmail.com',
+        area: 'ירושלים',
+        supplier: '54',
+        accountNumber: '62342-21'
+    },
+    {
+        id: '1241',
+        name: 'אליהו מזרחי',
+        phone: '054-5556666',
+        email: 'eliyahu.mizrahi@gmail.com',
+        area: 'צפון',
+        supplier: '50',
+        accountNumber: '62342-22'
+    },
+    {
+        id: '1242',
+        name: 'דינה קרויס',
+        phone: '050-7779999',
+        email: 'dina.krois@yahoo.com',
+        area: 'מרכז',
+        supplier: '51',
+        accountNumber: '62342-23'
+    },
+    {
+        id: '1243',
+        name: 'שלמה אזולאי',
+        phone: '053-9991111',
+        email: 'shlomo.azoulay@gmail.com',
+        area: 'דרום',
+        supplier: '52',
+        accountNumber: '62342-24'
+    },
+    {
+        id: '1244',
+        name: 'חנה ברקוביץ',
+        phone: '052-2225555',
+        email: 'hanna.berkowitz@walla.co.il',
+        area: 'השרון',
+        supplier: '53',
+        accountNumber: '62342-25'
+    },
+    {
+        id: '1245',
+        name: 'יצחק גולדברג',
+        phone: '054-4447777',
+        email: 'yitzhak.goldberg@gmail.com',
+        area: 'ירושלים',
+        supplier: '54',
+        accountNumber: '62342-26'
+    },
+    {
+        id: '1246',
+        name: 'אסתר כץ',
+        phone: '050-6669999',
+        email: 'esther.katz@bezeq.net',
+        area: 'צפון',
+        supplier: '50',
+        accountNumber: '62342-27'
+    },
+    {
+        id: '1247',
+        name: 'זכריה נחמני',
+        phone: '053-8881111',
+        email: 'zechariah.nahmani@hotmail.com',
+        area: 'מרכז',
+        supplier: '51',
+        accountNumber: '62342-28'
+    },
+    {
+        id: '1248',
+        name: 'יהודית סולומון',
+        phone: '052-1113333',
+        email: 'yehudit.solomon@gmail.com',
+        area: 'דרום',
+        supplier: '52',
+        accountNumber: '62342-29'
+    },
+    {
+        id: '1249',
+        name: 'בנימין שכטר',
+        phone: '054-3335555',
+        email: 'benjamin.shechter@yahoo.com',
+        area: 'השרון',
+        supplier: '53',
+        accountNumber: '62342-30'
+    },
+    {
+        id: '1250',
+        name: 'צפורה מלכה',
+        phone: '050-5557777',
+        email: 'tzipporah.malka@walla.co.il',
+        area: 'ירושלים',
+        supplier: '54',
+        accountNumber: '62342-31'
+    },
+    {
+        id: '1251',
+        name: 'גדעון וינר',
+        phone: '053-7779999',
+        email: 'gideon.weiner@gmail.com',
+        area: 'צפון',
+        supplier: '50',
+        accountNumber: '62342-32'
+    }, 
+    {
+        id: '1252',
+        name: 'מיכל רוזן',
+        phone: '052-4446666',
+        email: 'michal.rezen@gmail.com', 
+        area: 'מרכז',
+        supplier: '51',
+        accountNumber: '62342-33'
+    }
+];

2f0951c81f0572fabe354e65e6a3fc5e18bfe78d
Kobi Hari <kobi2294@yahoo.com>
2025-11-18 14:42:24 +0200

    table example

--- solar-ws/projects/main-app/src/app/pages/user/pages/washes/washes.component.ts
+++ solar-ws/projects/main-app/src/app/pages/user/pages/washes/washes.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { washColumns, washesData } from './washes.data';
+import { LibTableModule, SharedModule } from '@solar-lib';
 
 @Component({
   selector: 'app-washes',
-  imports: [],
+  imports: [SharedModule, LibTableModule],
   templateUrl: './washes.component.html',
   styleUrl: './washes.component.scss'
 })
 export default class WashesComponent {
+  readonly data = signal(washesData);
+  readonly keyword = signal('');
+
+  readonly filteredData = computed(() => this.data().filter(rec => 
+      rec.customer.includes(this.keyword()) 
+      || rec.id.toString().includes(this.keyword())
+  ));
+
+  readonly columns = signal(washColumns);
+  readonly showRate = signal(true);
+
+  readonly shownColumns = computed(() => this.showRate()
+    ? this.columns()
+    : this.columns().filter(col => col.id !== 'rate')
+  );
+
+  deleteRow(id: number) {
+    this.data.update(rows => rows.filter(r => r.id !== id));
+  }
+
+
 
 }

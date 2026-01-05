36f8e675fb1e3edeaab7c4ef98bd8180ca167e0e
Kobi Hari <kobi2294@yahoo.com>
2026-01-05 12:08:39 +0200

    added firebase functiona and deployed latest version to the cloud

--- solar-ws/functions/src/index.ts
+++ solar-ws/functions/src/index.ts
@@ -7,26 +7,23 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
+import { initializeApp } from "firebase-admin/app";
 import {setGlobalOptions} from "firebase-functions";
-import {onRequest} from "firebase-functions/https";
-import * as logger from "firebase-functions/logger";
+import { onCall } from "firebase-functions/v2/https";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+const region = 'me-west1';
+// const isEmulator = process.env.FUNCTIONS_EMULATOR === 'true';
+setGlobalOptions({
+  maxInstances: 50,
+  region,
+});
 
-// For cost control, you can set the maximum number of containers that can be
-// running at the same time. This helps mitigate the impact of unexpected
-// traffic spikes by instead downgrading performance. This limit is a
-// per-function limit. You can override the limit for each function using the
-// `maxInstances` option in the function's options, e.g.
-// `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
-// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1
-// functions should each use functions.runWith({ maxInstances: 10 }) instead.
-// In the v1 API, each function can only serve one request per container, so
-// this will be the maximum concurrent request count.
-setGlobalOptions({ maxInstances: 10 });
+initializeApp();
+const config = {
+    region
+}
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const sayHello = onCall<void, Promise<void>>(config, async req => {
+    console.log("Say, World!");    
+});
+

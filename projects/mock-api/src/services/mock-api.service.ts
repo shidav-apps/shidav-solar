8692e083249e9617aaa02d3ca0a5a9f2cad76be2
Kobi Hari <kobi2294@yahoo.com>
2026-01-07 16:57:50 +0200

    mock authentication now works with firebase auth

--- projects/mock-api/src/services/mock-api.service.ts
+++ projects/mock-api/src/services/mock-api.service.ts
@@ -1,59 +1,103 @@
-import { Api, Company, LoginResult, SolarReport, User } from '@contract';
-import { delay, Observable, of } from 'rxjs';
+import { Api, Company, LoginError, LoginResult, SolarReport, User } from '@contract';
+import { delay, from, map, Observable, of } from 'rxjs';
 import { MOCK_USERS } from '../data/users';
-import { MOCK_COMPANY_MAP } from '../data/company';
+import { MOCK_COMPANIES, MOCK_COMPANY_MAP } from '../data/company';
 import { mockCompanyToCompany } from './helpers';
 import { DashboardData } from '../../../contract/src/models/dashboard/dashboard-data.model';
 import { DataPeriod } from '../../../contract/src/models/data-period.model';
 import { getDataForSiteForPeriod } from '../data/dashboard-data';
 import { downloadCsv } from './downloader';
+import { inject } from '@angular/core';
+import { Auth, signInWithEmailAndPassword, signOut } from '@angular/fire/auth';
+import { FirebaseError } from '@angular/fire/app';
 
 export class MockApiService implements Api {
+  readonly auth = inject(Auth);
+
   login(
     req: { userid: string; password: string } | null
   ): Observable<LoginResult> {
     if (req === null) {
-      const userString = localStorage.getItem('solar-user');
-      if (userString) {
-        const user: User = JSON.parse(userString);
-        const res: LoginResult = { type: 'success', user };
-        return of(res);
-      }
-      return of({ type: 'not-init' });
+      // relogin
+      const res = new Observable<LoginResult>((subscriber) => {
+        const unsub = this.auth.onAuthStateChanged((user) => {
+          if (user) {
+            const mockUser = MOCK_USERS.find((u) => u.email === user.email);
+            if (mockUser) {
+              const companies: Company[] = mockUser.companyIds
+                .map((cid) => MOCK_COMPANY_MAP[cid])
+                .map(mockCompanyToCompany);
+
+              subscriber.next({
+                type: 'success',
+                user: {
+                  id: user.uid,
+                  email: user.email || '',
+                  displayName: mockUser.displayName,
+                  companies: companies,
+                },
+              });
+            }
+          } else {
+            subscriber.next({ type: 'not-init' });
+          }
+        });
+        return () => unsub();
+      });
+      return res;
     }
 
-    const mockUser = MOCK_USERS.find((u) => u.id === req.userid);
-    const found = !!mockUser && req.password === 'correct';
+    // if we got here, there is a login request
+    return new Observable<LoginResult>((subscriber) => {
+      const executer = async () => {
+        try {
+          const userCredential = await signInWithEmailAndPassword(
+            this.auth,
+            req.userid,
+            req.password
+          );
+          const id = userCredential.user.uid;
+          const email = userCredential.user.email || '';
+          const mockUser = MOCK_USERS.find((u) => u.email === email);
+          const displayName = mockUser ? mockUser.displayName : 'Unknown User';
+          const companies: Company[] = mockUser
+            ? mockUser.companyIds
+                .map((cid) => MOCK_COMPANY_MAP[cid])
+                .map(mockCompanyToCompany)
+            : [];
 
-    if (!found) {
-      const reason = !mockUser ? 'User Id Not Found' : 'Incorrect Password';
-      const res: LoginResult = { type: 'error', reason };
-      return of(res).pipe(delay(2000));
-    }
+          const res: LoginResult = {
+            type: 'success',
+            user: {
+              id: id,
+              email: email,
+              displayName: displayName,
+              companies: companies,
+            },
+          };
 
-    const companiesOfUser = mockUser.companyIds.map(
-      (id) => MOCK_COMPANY_MAP[id]
-    );
-    const companies: Company[] = companiesOfUser.map((mock) =>
-      mockCompanyToCompany(mock)
-    );
+          subscriber.next(res);
+        } catch (error) {
+          let reason: LoginError['reason'] = 'User Id Not Found';
+          if ((error instanceof FirebaseError) 
+              && error.code === 'auth/wrong-password') {
+            reason = 'Incorrect Password';
+          }
 
-    const user: User = {
-      id: mockUser.id,
-      email: mockUser.email,
-      displayName: mockUser.displayName,
-      companies: companies,
-    };
-
-    localStorage.setItem('solar-user', JSON.stringify(user));
-
-    const res: LoginResult = { type: 'success', user };
-    return of(res).pipe(delay(2000));
+          const res: LoginResult = {
+            type: 'error',
+            reason: reason,
+          };
+          subscriber.next(res);
+        }
+      };
+      executer();
+      return () => {};
+    });
   }
 
   logout(): Observable<void> {
-    localStorage.removeItem('solar-user');
-    return of(void 0).pipe(delay(1000));
+    return from(signOut(this.auth)).pipe(map(() => void 0));
   }
 
   getDashboardData(

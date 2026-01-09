37cb30b569042c7e29612a2a4a3771add92f99c8
Kobi Hari <kobi2294@yahoo.com>
2026-01-09 17:43:52 +0200

    real api service now uses firebase functions to fetch data

--- projects/real-api/src/services/real-api.service.ts
+++ projects/real-api/src/services/real-api.service.ts
@@ -1,5 +1,5 @@
 import { Api } from '@contract';
-import { Observable } from 'rxjs';
+import { firstValueFrom, Observable } from 'rxjs';
 import { Auth, User } from '@angular/fire/auth';
 import { MOCK_USERS } from '../../../mock-api/src/data/users';
 import { MOCK_COMPANY_MAP } from '../../../mock-api/src/data/company';
@@ -7,21 +7,22 @@ import { mockCompanyToCompany } from '../../../mock-api/src/services/helpers';
 import { fbAuth } from '@tools';
 import { inject } from '@angular/core';
 import { DbModel } from '@db-model';
+import { FirebaseFunctionsService } from './firebase-functions.service';
 
 export class RealApiService implements Api {
   readonly auth = inject(Auth);
+  readonly functions = inject(FirebaseFunctionsService);
 
-  async #mockOfUser(user: User) {
-    const mockUser = MOCK_USERS.find((u) => u.email === user.email);
-    const companies: DbModel.Company[] =
-      mockUser?.companyIds
-        .map((cid) => MOCK_COMPANY_MAP[cid])
-        .map(mockCompanyToCompany) || [];
+  async #userProfile(user: User) {
+    const dbUser = await firstValueFrom(
+      this.functions.getUserProfile(user.uid)
+    );
+
     return {
       id: user.uid,
       email: user.email || '',
-      displayName: mockUser ? mockUser.displayName : 'Unknown User',
-      companies: companies,
+      displayName: dbUser.displayName,
+      companies: dbUser.companies,
     };
   }
 
@@ -29,13 +30,13 @@ export class RealApiService implements Api {
     req: { userid: string; password: string } | null
   ): Observable<DbModel.LoginResult> {
     if (req === null)
-      return fbAuth.relogin(this.auth, (user) => this.#mockOfUser(user));
+      return fbAuth.relogin(this.auth, (user) => this.#userProfile(user));
 
     return fbAuth.loginWithCredentials(
       this.auth,
       req.userid,
       req.password,
-      (user) => this.#mockOfUser(user)
+      (user) => this.#userProfile(user)
     );
   }
 
@@ -43,12 +44,11 @@ export class RealApiService implements Api {
     return fbAuth.logout(this.auth);
   }
 
-
   getDashboardData(
     siteId: number,
     period: DbModel.DataPeriod
   ): Observable<DbModel.DashboardData> {
-    throw new Error('Method not implemented.');
+    return this.functions.getDashboardData({ siteId, period });
   }
 
   downloadReport(report: DbModel.SolarReport): Observable<void> {
